3a64c6235b1f841e7417531716090222b4fa6ad5
salmanmaq <salmanmaq@gmail.com>
2018-09-07 01:12:44 +0200

    Updated dataloader for training/testing

--- datasets/endovisDataLoader.py
+++ datasets/endovisDataLoader.py
@@ -10,23 +10,28 @@ from PIL import Image
 import os
 import json
 
-class miccaiSegDataset(Dataset):
+class endovisDataset(Dataset):
     '''
-        miccaiSeg Dataset
+        Endovis Dataset
     '''
 
-    def __init__(self, root_dir, transform=None, json_path=None):
+    def __init__(self, root_dir, transform=None, json_path=None, training=False):
         '''
         Args:
             root_dir (string): Directory with all the images
             transform(callable, optional): Optional transform to be applied on a sample
+            json_path (string, optional): Optional json path for dataset classes
+            training (bool): True when training, False for testing. If testing, skips loading
+                         the groundtruth
         '''
 
+        self.training = training
         self.root_dir = root_dir
         self.sub_dirs = [os.path.join(self.root_dir, sd) for sd in os.listdir(self.root_dir) \
                         if os.path.isdir(os.path.join(self.root_dir, sd))]
         self.img_dirs = [os.path.join(sd, 'left_images') for sd in self.sub_dirs]
-        self.gt_dirs = [os.path.join(sd, 'labels') for sd in self.sub_dirs]
+        if self.training:
+            self.gt_dirs = [os.path.join(sd, 'labels') for sd in self.sub_dirs]
         self.image_list = []
         for img_dir in self.img_dirs:
             self.image_list.append([f for f in os.listdir(img_dir) if (f.endswith('.png') or f.endswith('.jpg'))])
@@ -42,15 +47,20 @@ class miccaiSegDataset(Dataset):
 
     def __getitem__(self, idx):
         img_name = os.path.join(self.img_dir, self.image_list[idx])
-        gt_file_name = img_name.split('/')[-1]
-        gt_name = os.path.join(self.gt_dir, gt_file_name)
         image = Image.open(img_name)
         image = image.convert('RGB')
-        gt = Image.open(gt_name)
-        gt = gt.convert('RGB')
+        if not self.training:
+            gt_file_name = img_name.split('/')[-1]
+            gt_name = os.path.join(self.gt_dir, gt_file_name)
+            gt = Image.open(gt_name)
+            gt = gt.convert('RGB')
 
         if self.transform:
             image = self.transform(image)
-            gt = self.transform(gt)
+            if self.training:
+                gt = self.transform(gt)
 
-        return image, gt
+        if self.training:
+            return image, gt
+        else:
+            return image
